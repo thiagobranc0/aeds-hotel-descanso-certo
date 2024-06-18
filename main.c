53ffda20294c8f3b0be035ff6dda21a3d0805479
Thiago Branco <thiagobranco48@yahoo.com>
2024-06-17 23:10:59 -0300

    Add files via upload
    Criada a função de cadastrar cliente

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "./modules/hospedes.h"
+
+int main(void) {
+    
+    
+    cadastraCliente();
+    
+
+    return 0;
+}
---
+++ modules/hospedes.h
@@ -0,0 +1,50 @@
+#ifndef BIB1_H_INCLUDED
+#define BIB1_H_INCLUDED
+FILE *clientes;
+FILE *numeroClientes;
+FILE *funcionarios;
+FILE *numeroFuncionarios;
+FILE *quartos;
+FILE *numeroQuartos;
+
+void cadastraCliente() {
+    int id;
+    char nome[30], endereço[50], telefone[20];
+
+    system("md c:\\temp");
+
+    numeroClientes = fopen("c:/temp/numeroClientes.txt", "r");
+    if(numeroClientes == NULL) {
+        numeroClientes = fopen("c:/temp/numeroClientes.txt", "w");
+        fprintf(numeroClientes, "%d", 1);
+        fclose(numeroClientes);
+        id = 0;
+    } else {
+        fscanf(numeroClientes, "%d", &id);
+        fclose(numeroClientes);
+    }
+    id += 1;
+    numeroClientes = fopen("c:/temp/numeroClientes.txt", "w");
+    fprintf(numeroClientes, "%d", id);
+    fclose(numeroClientes);
+
+    clientes = fopen("c:/temp/clientes.txt", "a");
+    if(clientes == NULL) {
+        clientes = fopen("c:/temp/clientes.txt", "w");
+    }
+    fprintf(clientes, "%d\n", id);
+    printf("Digite o nome do cliente: ");
+    fgets(nome, 30, stdin);
+    fprintf(clientes, "%s", nome);
+    printf("Digite o endereco do cliente: ");
+    fgets(endereço, 50, stdin);
+    fprintf(clientes, "%s", endereço);
+    printf("Digite o telefone do cliente: ");
+    fgets(telefone, 20, stdin);
+    fprintf(clientes, "%s", telefone);
+
+    fclose(clientes);
+
+}
+
+#endif

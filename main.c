42339da672667a5070b38d32200df322d742ed93
Mirelly Pego Cordeiro de Alvarenga <mirellypego@gmail.com>
2024-06-23 19:30:19 -0300

    atualização

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <locale.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -5,8 +6,113 @@
 #include "./modules/funcionarios.h"
 #include "./modules/quartos.h"
 
-int main(void) {
-    EstadiasCliente();
+void menuHospede();
+void menuCadastro();
+
+/* TELA APENAS PARA CHAMAR FUNÇÕES DE CADASTROS ( F1, F2, F3 )*/
+void menuCadastro()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int escolha;
+
+    printf("---Escolha a Função Desejada---\n");
+    printf("1 - Cadastrar Cliente.\n");
+    printf("2 - Cadastrar Funcionário.\n");
+    printf("3 - Cadastrar Quarto.\n");
+    printf("0 - Sair.\n");
+
+    do
+    {
+        printf("Digite sua escolha: ");
+        scanf("%d", &escolha);
+
+        switch(escolha)
+        {
+        case 1:
+            cadastraCliente();
+            break;
+        case 2:
+            cadastraFuncionario();
+            break;
+        case 3:
+            cadastraQuarto();
+            break;
+        case 0:
+            printf("Finalizando atendimento...\n");
+            break;
+        default:
+            printf("Valor inválido.\nInsira uma opção disponível.\n");
+        }
+    } while (escolha != 0);
+}
+
+/* TELA DESTINADA A OPERAÇÕES QUE O HOSPEDE FARIA NO SISTEMA */
+void menuHospede()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int escolha;
+
+    printf("---Escolha a Função Desejada---\n");
+    printf("1 - Alugar quarto\n");
+    printf("2 - Total a pagar\n");
+    printf("3 - Pontos de fidelidade\n");
+    printf("0 - Sair\n");
+
+    do
+    {
+        printf("Digite sua escolha: ");
+        scanf("%d", &escolha);
+
+        switch (escolha)
+        {
+        case 1:
+            registraEstadia();
+            break;
+        case 2:
+            pagamento();
+            break;
+        case 3:
+            // fidelidade(); - INCOMPLETA
+            break;
+        case 0:
+            printf("Finalizando atendimento...\n");
+            break;
+        default:
+            printf("Valor inválido.\nInsira uma opção disponível.\n");
+        }
+    } while (escolha != 0);
+}
+
+/* TELA PRINCIPAL, APENAS PARA CHAMAR FUNÇÕES */
+int main(void)
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int escolha;
+    printf("ESCOLHA A OPÇÃO DESEJADA:\n");
+    printf("1 - Hóspedes\n");
+    printf("2 - Cadastros\n");
+
+    do
+    {
+        printf("Digite sua escolha: ");
+        scanf("%d", &escolha);
+
+        switch (escolha)
+        {
+        case 1:
+            menuHospede();
+            break;
+        case 2:
+            menuCadastro();
+            break;
+        default:
+            printf("Opção inválida. Insira um número válido.\n");
+            break;
+        }
+    } while (escolha != 0);
 
     return 0;
 }
--- modules/quartos.h
+++ modules/quartos.h
@@ -7,6 +7,16 @@ FILE *estadias;
 #ifndef BIB3_H_INCLUDED
 #define BIB3_H_INCLUDED
 
+void cadastraQuarto();
+int verificaDisponibilidade(int capacidade);
+int calculaDiarias(struct tm check_in, struct tm check_out);
+void registraEstadia();
+void salvaEstadia(int client_id, int quarto_id, int pessoas, struct tm check_in, struct tm check_out, int dias);
+void EstadiasCliente();
+void pagamento();
+int diasCliente(int cliente_id);
+float valorQuarto(int cliente_id);
+
 //Função 3:
 typedef struct
 {
@@ -289,4 +299,91 @@ void EstadiasCliente() {
     fclose(estadias);
 }
 
+
+void pagamento()
+{
+    int id_pessoa, quant_dias;
+    float valor_total, diarias, valor_quarto;
+
+    printf("Insira o seu número ID: ");
+    scanf("%d", &id_pessoa);
+
+    quant_dias = diasCliente(id_pessoa);
+    valor_quarto = valorQuarto(id_pessoa);
+
+    valor_total = quant_dias * valor_quarto;
+
+    printf("A quantidade total a se pagar é de R$%.2f\n", valor_total);
+}
+
+int diasCliente(int cliente_id)
+{
+    FILE *estadias = fopen("c:/temp/estadias.txt", "r");
+
+    if (estadias == NULL)
+    {
+        printf("Erro ao abrir o arquivo de estadias.\n");
+        return -1; // Retorna um valor negativo para indicar erro
+    }
+
+    Estadia estadia;
+    int encontrar = 0;
+    int dias = -1;
+
+    while (fscanf(estadias, "%d %d %d %d %d %d %d %d %d %d %d",
+                  &estadia.client_id, &estadia.quarto_id, &estadia.pessoas,
+                  &estadia.check_in_day, &estadia.check_in_month, &estadia.check_in_year,
+                  &estadia.check_out_day, &estadia.check_out_month, &estadia.check_out_year,
+                  &estadia.dias, &estadia.campo_de_erro) != EOF)
+    {
+        if (estadia.client_id == cliente_id)
+        {
+            dias = estadia.dias;
+            encontrar = 1;
+            break; // Sai do loop ao encontrar o cliente
+        }
+    }
+
+    fclose(estadias);
+
+    if (!encontrar)
+    {
+        printf("Nenhuma estadia encontrada para o cliente %d.\n", cliente_id);
+    }
+
+    return dias;
+}
+
+float valorQuarto(int cliente_id)
+{
+    FILE *quartos = fopen("c:/temp/quartos.txt", "r");
+    if (quartos == NULL)
+    {
+        printf("Erro ao abrir o arquivo dos quartos.\n");
+        return -1.0; // Retorna um valor negativo para indicar erro
+    }
+
+    quarto q;
+    float preco = -1.0;
+    int encontrar = 0;
+
+    while (fscanf(quartos, "%d%d%f%d", &q.numero, &q.capacidade, &q.preco, &q.ocupado) != EOF)
+    {
+        if (q.numero == cliente_id)
+        {
+            preco = q.preco;
+            encontrar = 1;
+            break; // Sai do loop ao encontrar o quarto
+        }
+    }
+
+    fclose(quartos);
+
+    if (!encontrar)
+    {
+        printf("Nenhum quarto encontrado para o cliente %d.\n", cliente_id);
+    }
+
+    return preco;
+}
 #endif
